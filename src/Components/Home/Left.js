3128c5f21b4b81452c6a70c38521c08a7bc9413c
syed-jaid <syed.jaid.7748@gmail.com>
2022-09-23 11:51:14 +0600

    nav , home page ui is allmost done

--- src/Components/Home/Left.js
+++ src/Components/Home/Left.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { CgProfile } from 'react-icons/cg';
+import { BsChatRightDots } from 'react-icons/bs';
 
 const Left = () => {
     return (
@@ -6,19 +8,19 @@ const Left = () => {
             <ul class="menu bg-base-100 w-56 mt-2 border-[4px] border-[#cbd5e1] rounded-xl  font-semibold">
                 <li className='border-b-[2px] border-base-400'>
                     <a href='/'>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-[21px] w-[21px]" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
                         HOME
                     </a>
                 </li>
                 <li className=''>
                     <a href='/'>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <CgProfile className='text-[20px]' />
                         PROFILE
                     </a>
                 </li>
                 <li className='border-t-[2px] border-base-400'>
                     <a href='/'>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+                        <BsChatRightDots className='text-[17px]' />
                         CHAT
                     </a>
                 </li>
